cbddc0ec269c4d9e06d13dc23b55e4df77f1a8eb
Charlie DeRiggs <charlie.deriggs@gmail.com>
2017-09-04 16:00:26 -0400

    sql

--- autocms.sql
+++ autocms.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 04, 2017 at 08:37 PM
--- Server version: 5.6.35
--- PHP Version: 7.0.20
+-- Generation Time: Sep 04, 2017 at 07:59 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -58,15 +60,6 @@ CREATE TABLE `customers` (
   `lastVisited` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`customerid`, `firstname`, `lastname`, `customeremail`, `customerphone`, `city`, `parish`, `vehiclereg`, `lastVisited`) VALUES
-(172, '', '', '', '', '', '', '', '2017-08-26-37'),
-(173, 'charlie', 'deriggs', 'me@me.com', '4734055738', 'marrast', 'saint geo', 'pm 33', '2017-09-02-41'),
-(174, 'charlie', 'deriggs', 'me@me.com', '4734055738', 'marrast', 'saint geo', 'pm 33', '2017-09-02-41');
-
 -- --------------------------------------------------------
 
 --
@@ -77,22 +70,12 @@ CREATE TABLE `invoices` (
   `invoiceid` int(11) NOT NULL,
   `customerid` int(11) NOT NULL,
   `invoice_date` varchar(255) NOT NULL,
+  `mileage` varchar(255) NOT NULL,
+  `note` varchar(800) NOT NULL,
   `total` decimal(10,0) NOT NULL,
   `status` varchar(255) NOT NULL,
   `file` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `invoices`
---
-
-INSERT INTO `invoices` (`invoiceid`, `customerid`, `invoice_date`, `total`, `status`, `file`) VALUES
-(240, 172, '', '0', 'open', '--2017-08-26-37'),
-(241, 172, '', '0', 'open', '--2017-08-26-56'),
-(242, 172, '', '0', 'open', '--2017-08-26-26'),
-(243, 173, '', '96', 'open', 'deriggs-charlie-2017-09-02-41'),
-(244, 173, '', '319', 'open', 'deriggs-charlie-2017-09-02-57'),
-(245, 174, '', '319', 'open', 'deriggs-charlie-2017-09-02-57');
 
 -- --------------------------------------------------------
 
@@ -113,72 +96,41 @@ CREATE TABLE `invoice_items` (
 --
 
 INSERT INTO `invoice_items` (`id`, `product`, `qty`, `price`, `subtotal`) VALUES
-(292, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(291, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(290, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(288, '', 1, '', '0.00'),
-(289, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(286, '', 1, '', '0.00'),
-(287, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(285, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(284, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(283, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(282, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(281, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(280, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(278, '', 1, '', '0.00'),
-(279, '', 1, '', '0.00'),
-(277, '', 1, '', '0.00'),
-(276, '', 1, '', '0.00'),
-(275, '', 1, '', '0.00'),
-(273, '', 1, '', '0.00'),
-(274, '', 1, '', '0.00'),
-(271, '', 1, '', '0.00'),
-(272, '', 1, '', '0.00'),
-(270, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(269, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(268, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(267, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(266, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(265, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(264, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(263, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(262, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(259, '', 1, '', '0.00'),
-(260, '', 1, '', '0.00'),
-(261, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(258, '', 1, '', '0.00'),
-(257, '', 1, '', '0.00'),
-(256, '', 1, '', '0.00'),
-(255, '', 1, '', '0.00'),
-(254, '', 1, '', '0.00'),
-(253, '', 1, '', '0.00'),
-(252, '', 1, '', '0.00'),
-(251, '', 1, '', '0.00'),
-(250, '', 1, '', '0.00'),
-(249, '', 1, '', '0.00'),
-(248, '', 1, '', '0.00'),
-(247, '', 1, '', '0.00'),
-(246, '', 1, '', '0.00'),
-(245, '', 1, '', '0.00'),
-(244, '', 1, '', '0.00'),
-(243, '', 1, '', '0.00'),
-(242, '', 1, '', '0.00'),
-(241, '', 1, '', '0.00'),
-(240, '', 1, '', '0.00'),
-(239, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(238, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(237, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(236, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(235, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(293, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(294, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(295, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(296, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99'),
-(297, '', 1, '', '0.00'),
-(298, '', 1, '', '0.00'),
-(299, '', 1, '', '0.00'),
-(300, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99');
+(361, '', 1, '', '0.00'),
+(360, '', 1, '', '0.00'),
+(359, '', 1, '', '0.00'),
+(358, '', 1, '', '0.00'),
+(357, '', 1, '', '0.00'),
+(356, '', 1, '', '0.00'),
+(355, '', 1, '', '0.00'),
+(354, '', 1, '', '0.00'),
+(353, '', 1, '', '0.00'),
+(352, '', 1, '', '0.00'),
+(351, '', 1, '', '0.00'),
+(350, '', 1, '', '0.00'),
+(349, '', 1, '', '0.00'),
+(348, '', 1, '', '0.00'),
+(347, '', 1, '', '0.00'),
+(346, '', 1, '', '0.00'),
+(345, '', 1, '', '0.00'),
+(344, '', 1, '', '0.00'),
+(343, '', 1, '', '0.00'),
+(342, '', 1, '', '0.00'),
+(341, '', 1, '', '0.00'),
+(340, '', 1, '', '0.00'),
+(339, '', 1, '', '0.00'),
+(338, '', 1, '', '0.00'),
+(337, '', 1, '', '0.00'),
+(336, '', 1, '', '0.00'),
+(335, '', 1, '', '0.00'),
+(334, '', 1, '', '0.00'),
+(333, '', 1, '', '0.00'),
+(332, '', 1, '', '0.00'),
+(330, '', 1, '', '0.00'),
+(331, '', 1, '', '0.00'),
+(328, '', 1, '', '0.00'),
+(329, '', 1, '', '0.00'),
+(327, 'Brake Fluid - Castrol GTX - 95.99', 1, '95.99', '95.99');
 
 -- --------------------------------------------------------
 
@@ -198,7 +150,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`productcode`, `productname`, `productdesc`, `unitprice`) VALUES
-(0, 'Brake Fluid', 'Castrol GTX', '95.99');
+(0, 'Brake Fluid', 'Castrol GTX', 95.99);
 
 -- --------------------------------------------------------
 
@@ -248,22 +200,23 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customerid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
+  MODIFY `customerid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=197;
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `invoiceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
+  MODIFY `invoiceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=313;
 --
 -- AUTO_INCREMENT for table `invoice_items`
 --
 ALTER TABLE `invoice_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=301;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=362;
 --
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehicle_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `vehicle_id` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
